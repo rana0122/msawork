2c2a06d6c17504d0d3362f6e0120ba270cfc0666
rana0122 <rana0122@naver.com>
2025-01-13 23:06:07 +0900

    cloud bus

--- UserService/src/main/java/com/example/userservice/vo/RequestUser.java
+++ UserService/src/main/java/com/example/userservice/vo/RequestUser.java
@@ -2,10 +2,10 @@ package com.example.userservice.vo;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Data
 public class RequestUser {
